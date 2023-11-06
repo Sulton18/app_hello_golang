8df4de0795e97c91a1ef5f0bfd62498f5b4178de
sulton darmawan <sulton.darmawan@dwidasa.com>
2023-11-06 11:59:03 +0700

    membuat modul main go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Sulton18/app_hello_golang.git
+
+go 1.13
+
+require github.com/Sulton18/hello_golang v1.0.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	go_hello "github.com/Sulton18/hello_golang"
+)
+
+func main() {
+	fmt.Println(go_hello.HelloGo())
+}
